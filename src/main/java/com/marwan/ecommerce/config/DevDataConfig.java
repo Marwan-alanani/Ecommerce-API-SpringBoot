d849f4076e1db0bdb3bb1d20b533b44345c8d7e6
Marwan <marwan-walid1@hotmail.com>
2026-02-11 20:33:40 +0200

    refactor data seeding

--- src/main/java/com/marwan/ecommerce/config/DevDataConfig.java
+++ src/main/java/com/marwan/ecommerce/config/DevDataConfig.java
@@ -27,151 +27,151 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DevDataConfig
 {
-
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final CategoryService categoryService;
+    private final PasswordEncoder passwordEncoder;
     @Value("${admin.email}")
     private String adminEmail;
     @Value("${admin.password}")
     private String adminPassword;
-    private final String userEmail = "defaultuser@mail.com";
+    private static final String USEREMAIL = "defaultuser@mail.com";
 
     @Bean
     @Transactional
-    CommandLineRunner seedUserData(UserRepository userRepository, PasswordEncoder passwordEncoder)
+    CommandLineRunner initDatabase()
     {
         return args -> {
-            if (!userRepository.existsByEmail(adminEmail)) {
-                User admin = User.create(
-                        "admin",
-                        "admin",
-                        UserRole.ADMIN,
-                        adminEmail,
-                        passwordEncoder.encode(adminPassword)
-                );
-                userRepository.save(admin);
-
-            }
-            if (!userRepository.existsByEmail(userEmail)) {
-                User user = User.create(
-                        "user",
-                        "user",
-                        UserRole.USER,
-                        userEmail,
-                        passwordEncoder.encode("password")
-                );
-                userRepository.save(user);
-            }
+            seedUserData();
+            seedCategoryData();
+            seedProductData();
         };
     }
 
-    @Bean
-    @Transactional
-    CommandLineRunner seedCategoryData(CategoryService categoryService,
-            CategoryRepository categoryRepository)
+    private void seedUserData()
     {
-        return args -> {
-            if (categoryRepository.count() == 0) {
-                List<CreateCategoryCommand> commands = new ArrayList<>(List.of(
-                        new CreateCategoryCommand("Furniture"),
-                        new CreateCategoryCommand("Fashion"),
-                        new CreateCategoryCommand("Electronics")
-                )
+        if (!userRepository.existsByEmail(adminEmail)) {
+            User admin = User.create(
+                    "admin",
+                    "admin",
+                    UserRole.ADMIN,
+                    adminEmail,
+                    passwordEncoder.encode(adminPassword)
+            );
+            userRepository.save(admin);
 
-                );
+        }
+        if (!userRepository.existsByEmail(USEREMAIL)) {
+            User user = User.create(
+                    "user",
+                    "user",
+                    UserRole.USER,
+                    USEREMAIL,
+                    passwordEncoder.encode("password")
+            );
+            userRepository.save(user);
+        }
 
-                for (CreateCategoryCommand command : commands) {
-                    categoryService.create(command);
-                }
-            }
-        };
     }
 
-    @Bean
-    @Transactional
-    CommandLineRunner seedProductData(ProductRepository productRepository,
-            CategoryRepository categoryRepository)
+    private void seedCategoryData()
     {
-        return args -> {
-            if (productRepository.count() == 0) {
-                Category furniture = categoryRepository.findByName("Furniture").orElse(null);
-                Category fashion = categoryRepository.findByName("Fashion").orElse(null);
-                Category electronics = categoryRepository.findByName("Electronics").orElse(null);
+        if (categoryRepository.count() == 0) {
+            List<CreateCategoryCommand> commands = new ArrayList<>(List.of(
+                    new CreateCategoryCommand("Furniture"),
+                    new CreateCategoryCommand("Fashion"),
+                    new CreateCategoryCommand("Electronics")
+            )
 
-                List<Product> products = new ArrayList<>(List.of(
-                        Product.create(
-                                "Bed",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic" +
-                                        ".com/images?q=tbn:ANd9GcSAjEccafdE29oPjq8fXKg2K0HM6_x63WTL9g&s",
-                                furniture
-                        ),
-                        Product.create(
-                                "Drawer",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt_B7WVTx-g0xk63Y5NyCOdtufTn6MtTmGUg&s",
-                                furniture
-                        ),
+            );
 
-                        Product.create(
-                                "Desk",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://www.officestock.com.au/assets/full/CDK126BI.jpg?20210405235659",
-                                furniture
-                        ),
-                        Product.create(
-                                "T-shirt",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic" +
-                                        ".com/images?q=tbn:ANd9GcSuFDcsZpcD7gF-zD8NQU_N7SSzoOQKrXvThQ&s",
-                                fashion
-                        ),
-
-                        Product.create(
-                                "Jeans",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rpOb-4Qw9zEYHDdo3_N7tYczkwZzO4LLrQ&s",
-                                fashion
-                        ),
-
-                        Product.create(
-                                "Jacket",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS9yaUixpCxwbJIHxVgr-7imMUuAlq3J_-DQ&s",
-                                fashion
-                        ),
-                        Product.create(
-                                "TV",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uPThN2OG8iYPzlCOEmGCvGqy2euKbBVTRQ&s",
-                                electronics
-                        ),
-                        Product.create(
-                                "Mobile Phone",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://2b.com.eg/media/catalog/product/cache/45bcba66b667d1ca52af48b101a5f0cb/a/p/apple-iphone-16-pro-black-1_1_1_2_1.jpg",
-                                electronics
-                        ),
-                        Product.create(
-                                "Laptop",
-                                "",
-                                BigDecimal.valueOf(10.2),
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxd9zq7NatrST5edevjeknTKTKmuL5ElwDag&s",
-                                electronics
-                        )
-
-                ));
-                for (Product product : products) {
-                    productRepository.save(product);
-                }
+            for (CreateCategoryCommand command : commands) {
+                categoryService.create(command);
             }
-        };
+        }
     }
 
+    private void seedProductData()
+    {
+        if (productRepository.count() == 0) {
+            Category furniture =
+                    categoryRepository.findByName("Furniture").orElse(null);
+            Category fashion = categoryRepository.findByName("Fashion").orElse(null);
+            Category electronics = categoryRepository.findByName("Electronics").orElse(null);
+
+            List<Product> products = new ArrayList<>(List.of(
+                    Product.create(
+                            "Bed",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic" +
+                                    ".com/images?q=tbn:ANd9GcSAjEccafdE29oPjq8fXKg2K0HM6_x63WTL9g&s",
+                            furniture
+                    ),
+                    Product.create(
+                            "Drawer",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt_B7WVTx-g0xk63Y5NyCOdtufTn6MtTmGUg&s",
+                            furniture
+                    ),
+
+                    Product.create(
+                            "Desk",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://www.officestock.com.au/assets/full/CDK126BI.jpg?20210405235659",
+                            furniture
+                    ),
+                    Product.create(
+                            "T-shirt",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic" +
+                                    ".com/images?q=tbn:ANd9GcSuFDcsZpcD7gF-zD8NQU_N7SSzoOQKrXvThQ&s",
+                            fashion
+                    ),
+
+                    Product.create(
+                            "Jeans",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rpOb-4Qw9zEYHDdo3_N7tYczkwZzO4LLrQ&s",
+                            fashion
+                    ),
+
+                    Product.create(
+                            "Jacket",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS9yaUixpCxwbJIHxVgr-7imMUuAlq3J_-DQ&s",
+                            fashion
+                    ),
+                    Product.create(
+                            "TV",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uPThN2OG8iYPzlCOEmGCvGqy2euKbBVTRQ&s",
+                            electronics
+                    ),
+                    Product.create(
+                            "Mobile Phone",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://2b.com.eg/media/catalog/product/cache/45bcba66b667d1ca52af48b101a5f0cb/a/p/apple-iphone-16-pro-black-1_1_1_2_1.jpg",
+                            electronics
+                    ),
+                    Product.create(
+                            "Laptop",
+                            "",
+                            BigDecimal.valueOf(10.2),
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxd9zq7NatrST5edevjeknTKTKmuL5ElwDag&s",
+                            electronics
+                    )
+
+            ));
+            productRepository.saveAll(products);
+        }
+    }
 }
