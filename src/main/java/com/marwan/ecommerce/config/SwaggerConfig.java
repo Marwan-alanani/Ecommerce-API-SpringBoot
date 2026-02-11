2bee664dc2825a26fc4a82b4c86fcc16d7f9ea1c
Marwan <marwan-walid1@hotmail.com>
2026-02-11 21:02:49 +0200

    Added url in customOpenApi

--- src/main/java/com/marwan/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/marwan/ecommerce/config/SwaggerConfig.java
@@ -5,18 +5,24 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig
 {
 
     @Bean
-    public OpenAPI customOpenAPI()
+    public OpenAPI customOpenAPI(@Value("${app.url:http://localhost:8080}") String url)
+
     {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .servers(List.of(new Server().url(url).description("Deployed Server")))
                 .info(new Info()
                         .title("My Project API")
                         .version("1.0")
