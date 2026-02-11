9206cb61a35e6aefdd2ab5bc5804f587bf6a406d
Marwan <marwan-walid1@hotmail.com>
2026-02-11 18:46:12 +0200

    Data seeding only when db is empty

--- src/main/java/com/marwan/ecommerce/ECommerceApplication.java
+++ src/main/java/com/marwan/ecommerce/ECommerceApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceApplication
 {
-
-
     static void main(String[] args)
     {
         SpringApplication.run(ECommerceApplication.class, args);
     }
-
 }
--- src/main/java/com/marwan/ecommerce/config/DevDataConfig.java
+++ src/main/java/com/marwan/ecommerce/config/DevDataConfig.java
@@ -59,19 +59,22 @@ public class DevDataConfig
     }
 
     @Bean
-    CommandLineRunner seedCategoryData(CategoryService categoryService)
+    CommandLineRunner seedCategoryData(CategoryService categoryService,
+            CategoryRepository categoryRepository)
     {
         return args -> {
-            List<CreateCategoryCommand> commands = new ArrayList<>(List.of(
-                    new CreateCategoryCommand("Furniture"),
-                    new CreateCategoryCommand("Fashion"),
-                    new CreateCategoryCommand("Electronics")
-            )
+            if (categoryRepository.count() == 0) {
+                List<CreateCategoryCommand> commands = new ArrayList<>(List.of(
+                        new CreateCategoryCommand("Furniture"),
+                        new CreateCategoryCommand("Fashion"),
+                        new CreateCategoryCommand("Electronics")
+                )
 
-            );
+                );
 
-            for (CreateCategoryCommand command : commands) {
-                categoryService.create(command);
+                for (CreateCategoryCommand command : commands) {
+                    categoryService.create(command);
+                }
             }
         };
     }
@@ -81,83 +84,85 @@ public class DevDataConfig
             CategoryRepository categoryRepository)
     {
         return args -> {
-            Category furniture = categoryRepository.findByName("Furniture").orElse(null);
-            Category fashion = categoryRepository.findByName("Fashion").orElse(null);
-            Category electronics = categoryRepository.findByName("Electronics").orElse(null);
+            if (productRepository.count() == 0) {
+                Category furniture = categoryRepository.findByName("Furniture").orElse(null);
+                Category fashion = categoryRepository.findByName("Fashion").orElse(null);
+                Category electronics = categoryRepository.findByName("Electronics").orElse(null);
 
-            List<Product> products = new ArrayList<>(List.of(
-                    Product.create(
-                            "Bed",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic" +
-                                    ".com/images?q=tbn:ANd9GcSAjEccafdE29oPjq8fXKg2K0HM6_x63WTL9g&s",
-                            furniture
-                    ),
-                    Product.create(
-                            "Drawer",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt_B7WVTx-g0xk63Y5NyCOdtufTn6MtTmGUg&s",
-                            furniture
-                    ),
+                List<Product> products = new ArrayList<>(List.of(
+                        Product.create(
+                                "Bed",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic" +
+                                        ".com/images?q=tbn:ANd9GcSAjEccafdE29oPjq8fXKg2K0HM6_x63WTL9g&s",
+                                furniture
+                        ),
+                        Product.create(
+                                "Drawer",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt_B7WVTx-g0xk63Y5NyCOdtufTn6MtTmGUg&s",
+                                furniture
+                        ),
 
-                    Product.create(
-                            "Desk",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://www.officestock.com.au/assets/full/CDK126BI.jpg?20210405235659",
-                            furniture
-                    ),
-                    Product.create(
-                            "T-shirt",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic" +
-                                    ".com/images?q=tbn:ANd9GcSuFDcsZpcD7gF-zD8NQU_N7SSzoOQKrXvThQ&s",
-                            fashion
-                    ),
+                        Product.create(
+                                "Desk",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://www.officestock.com.au/assets/full/CDK126BI.jpg?20210405235659",
+                                furniture
+                        ),
+                        Product.create(
+                                "T-shirt",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic" +
+                                        ".com/images?q=tbn:ANd9GcSuFDcsZpcD7gF-zD8NQU_N7SSzoOQKrXvThQ&s",
+                                fashion
+                        ),
 
-                    Product.create(
-                            "Jeans",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rpOb-4Qw9zEYHDdo3_N7tYczkwZzO4LLrQ&s",
-                            fashion
-                    ),
+                        Product.create(
+                                "Jeans",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rpOb-4Qw9zEYHDdo3_N7tYczkwZzO4LLrQ&s",
+                                fashion
+                        ),
 
-                    Product.create(
-                            "Jacket",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS9yaUixpCxwbJIHxVgr-7imMUuAlq3J_-DQ&s",
-                            fashion
-                    ),
-                    Product.create(
-                            "TV",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uPThN2OG8iYPzlCOEmGCvGqy2euKbBVTRQ&s",
-                            electronics
-                    ),
-                    Product.create(
-                            "Mobile Phone",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://2b.com.eg/media/catalog/product/cache/45bcba66b667d1ca52af48b101a5f0cb/a/p/apple-iphone-16-pro-black-1_1_1_2_1.jpg",
-                            electronics
-                    ),
-                    Product.create(
-                            "Laptop",
-                            "",
-                            BigDecimal.valueOf(10.2),
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxd9zq7NatrST5edevjeknTKTKmuL5ElwDag&s",
-                            electronics
-                    )
+                        Product.create(
+                                "Jacket",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS9yaUixpCxwbJIHxVgr-7imMUuAlq3J_-DQ&s",
+                                fashion
+                        ),
+                        Product.create(
+                                "TV",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uPThN2OG8iYPzlCOEmGCvGqy2euKbBVTRQ&s",
+                                electronics
+                        ),
+                        Product.create(
+                                "Mobile Phone",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://2b.com.eg/media/catalog/product/cache/45bcba66b667d1ca52af48b101a5f0cb/a/p/apple-iphone-16-pro-black-1_1_1_2_1.jpg",
+                                electronics
+                        ),
+                        Product.create(
+                                "Laptop",
+                                "",
+                                BigDecimal.valueOf(10.2),
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxd9zq7NatrST5edevjeknTKTKmuL5ElwDag&s",
+                                electronics
+                        )
 
-            ));
-            for (Product product : products) {
-                productRepository.save(product);
+                ));
+                for (Product product : products) {
+                    productRepository.save(product);
+                }
             }
         };
     }
